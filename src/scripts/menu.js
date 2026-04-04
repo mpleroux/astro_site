98dfb99d821b4b93a30604822d63fd2fdf39c52b
Michael LeRoux <946718+mpleroux@users.noreply.github.com>
2026-04-03 22:23:28 -0400

    Add subtle drop shadow to mobile menu when it's open

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -5,4 +5,5 @@ menu?.addEventListener("click", () => {
   const isExpanded = menu.getAttribute("aria-expanded") === "true";
   menu.setAttribute("aria-expanded", `${!isExpanded}`);
   navMenu?.classList.toggle("hidden");
+  navMenu?.classList.toggle("shadow-md");
 });
